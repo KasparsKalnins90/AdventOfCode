8bb08ff82a15461b4098782d1e492d8fd2a8bd65
Kaspars Kalnins <joxxxter@gmail.com>
2023-12-03 14:03:46 +0200

    Day 2 star 1 done

--- CodeRunner/Program.cs
+++ CodeRunner/Program.cs
@@ -1,7 +1,8 @@
 ﻿using Y2023.Day1;
+using Y2023.Day2;
 using Y2023.Input;
-var fileReader = new InputFileReader("Day1.txt", "\r\n");
-var coordinateList = fileReader.GetItems();
-    //Console.WriteLine(CalibrationDocumentReader.SumCoordinates(coordinateList));
-Console.WriteLine(CalibrationDocumentReader.SumCoordinatesImproved(coordinateList));
+var fileReader = new InputFileReader("Day2.txt", "\r\n");
+var gameInfos = fileReader.GetItems();
+//Console.WriteLine(CalibrationDocumentReader.SumCoordinates(coordinateList));
+Console.WriteLine(CubeGameValidator.Validate(gameInfos));
 
---
+++ Y2023/Day2/CubeGame.cs
@@ -0,0 +1,27 @@
+﻿namespace Y2023.Day2;
+
+internal class CubeGame
+{
+    public int Id { get; set; }
+    public Dictionary<string, int> cubeInfo{ get; set; }
+    public CubeGame(string gameInfo)
+    {
+        cubeInfo = new Dictionary<string, int>();
+        var SplitGameAndId = gameInfo.Split(':');
+        Id = int.Parse(SplitGameAndId[0].Split(' ')[1]);
+        var games = SplitGameAndId[1].Replace(';', ',').Split(",");
+        foreach(var game in games)
+        {
+            var amouuntAndColor = game.Trim().Split(' ');
+            var color = amouuntAndColor[1];
+            var amount = int.Parse(amouuntAndColor[0]);
+            if (!cubeInfo.ContainsKey(color))
+            {
+                cubeInfo.Add(color, amount);
+            } else if (cubeInfo[color] < amount)
+            {
+                cubeInfo[color] = amount;
+            }
+        }
+    }
+}
