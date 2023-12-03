5d70b6df68383230872fa884ab5e1127881ee857
Kaspars Kalnins <joxxxter@gmail.com>
2023-12-03 14:03:46 +0200

    Day 2 star 1 done

---
+++ Y2023/Day2/CubeGameValidator.cs
@@ -0,0 +1,17 @@
+﻿namespace Y2023.Day2;
+
+internal static class CubeGameValidator
+{
+    public static int Validate(List<string> cubeGames)
+    {
+        var games = cubeGames.Select(game => new CubeGame(game)).ToList();
+        //12 red cubes, 13 green cubes, and 14 blue cubes
+        var result = games.Where(game => 
+        game.cubeInfo["red"] <= 12 
+        && game.cubeInfo["green"] <= 13
+        && game.cubeInfo["blue"] <= 14)
+            .Select(game => game.Id)
+            .Sum();
+        return result;
+    }
+}
