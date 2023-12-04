b84dea607d9d244195bc3c1b43cb5e20df0d7875
Kaspars Kalnins <joxxxter@gmail.com>
2023-12-05 00:22:40 +0200

    day3 star 2 done

--- CodeRunner/Program.cs
+++ CodeRunner/Program.cs
@@ -2,6 +2,6 @@
 using Y2023.Input;
 var fileReader = new InputFileReader("Day3.txt", "\r\n");
 var gameInfos = fileReader.GetItems();
-Console.WriteLine(EngineTools.GetSerialNumber(gameInfos));
+Console.WriteLine(EngineTools.GetGearRatio  (gameInfos));
 
 
