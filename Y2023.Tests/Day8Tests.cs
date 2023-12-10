206ced2d0530ae1bcf0623cb6b63d5b32ef86c79
Kaspars Kalnins <kaspars.kalnins@i-standards.com>
2023-12-11 01:16:19 +0200

    day 8 star 2 possible solution

--- Y2023.Tests/Day8Tests.cs
+++ Y2023.Tests/Day8Tests.cs
@@ -18,4 +18,23 @@ public class Day8Tests
     {
         DesertNavigator.Navigate(_input).Should().Be(6);
     }
+
+    [Fact]
+    public void NavigateBetter_ShouldReturnExpected()
+    {
+        var betterInput = new List<string>()
+        {
+            "LR",
+            "",
+            "11A = (11B, XXX)",
+            "11B = (XXX, 11Z)",
+            "11Z = (11B, XXX)",
+            "22A = (22B, XXX)",
+            "22B = (22C, 22C)",
+            "22C = (22Z, 22Z)",
+            "22Z = (22B, 22B)",
+            "XXX = (XXX, XXX)",
+        };
+        DesertNavigator.NavigateBetter(betterInput).Should().Be(6);
+    }
 }
