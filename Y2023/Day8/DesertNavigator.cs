f784e688d752183312787f9fd4b179cedc11e6ca
Kaspars Kalnins <kaspars.kalnins@i-standards.com>
2023-12-11 11:13:20 +0200

    day 8 star 2 done

--- Y2023/Day8/DesertNavigator.cs
+++ Y2023/Day8/DesertNavigator.cs
@@ -1,14 +1,7 @@
-using System.Text;
 using System.Text.RegularExpressions;
 using Y2023.Input;
 
 namespace Y2023.Day8;
-
-internal class NodeInfo
-{
-    public string Left { get; set; }
-    public string Right { get; set; }
-}
 
 internal static class DesertNavigator
 {
@@ -16,103 +9,117 @@ internal static class DesertNavigator
 
     private static readonly Regex _capitalLettersAndDigitsRegex = new Regex("([a-zA-Z0-9]+)");
 
-    public static int NavigateBetter(List<string> navigationInstructions)
-        {
-            var directions = navigationInstructions[0];
-            var nodes = new List<DesertNavigatorNode>();
-            for (int i = 2; i < navigationInstructions.Count; i++)
-            {
-                var matches = _capitalLettersAndDigitsRegex.Matches(navigationInstructions[i]);
-                nodes.Add(new DesertNavigatorNode
-                {
-                    Key = matches[0].Value,
-                    Left = matches[1].Value,
-                    Right = matches[2].Value
-                });
-            }
-
-            var startingNodes = nodes.Where(node => node.Key[2] == 'A').ToList();
-
-            var currentNodes = startingNodes;
-            var moves = 0;
-            while (true)
-            {
-                foreach (var direction in directions)
-                {
-                    if (moves % 1000000 == 0)
-                    {
-                        Console.WriteLine($"moves : {moves}");
-                    }
-
-                    moves++;
-                    if (direction == 'L')
-                    {
-                        currentNodes =
-                            currentNodes.Select(currentNode => nodes.First(node => node.Key == currentNode.Left))
-                                .ToList();
-                    }
-
-                    if (direction == 'R')
-                    {
-                        currentNodes =
-                            currentNodes.Select(currentNode => nodes.First(node => node.Key == currentNode.Right))
-                                .ToList();
-                    }
-
-                    if (currentNodes.All(currentNode => currentNode.Key[2] == 'Z'))
-                    {
-                        return moves;
-                    }
-                }
-            }
-
-            return 0;
-        }
-
-        public static int Navigate(List<string> navigationInstructions)
-        {
-            var directions = navigationInstructions[0];
-            var nodes = new List<DesertNavigatorNode>();
-            for (int i = 2; i < navigationInstructions.Count; i++)
-            {
-                var matches = _capitalLettersRegex.Matches(navigationInstructions[i]);
-                nodes.Add(new DesertNavigatorNode
-                {
-                    Key = matches[0].Value,
-                    Left = matches[1].Value,
-                    Right = matches[2].Value
-                });
-            }
-
-            var currentNode = nodes.First(node => node.Key == "AAA");
-            var moves = 0;
-            while (true)
-            {
-                foreach (var direction in directions)
-                {
-                    moves++;
-                    if (direction == 'L')
-                    {
-                        currentNode = nodes.First(node => node.Key == currentNode.Left);
-                    }
-
-                    if (direction == 'R')
-                    {
-                        currentNode = nodes.First(node => node.Key == currentNode.Right);
-                    }
-
-                    if (currentNode.Key == "ZZZ")
-                    {
-                        return moves;
-                    }
-                }
-            }
-        }
-    }
-
-    internal class DesertNavigatorNode
+    public static decimal NavigateBetter(List<string> navigationInstructions)
     {
-        public string Key { get; set; }
-        public string Left { get; set; }
-        public string Right { get; set; }
+        var directions = navigationInstructions[0];
+        var nodes = new List<DesertNavigatorNode>();
+        for (var i = 2; i < navigationInstructions.Count; i++)
+        {
+            var matches = _capitalLettersAndDigitsRegex.Matches(navigationInstructions[i]);
+            nodes.Add(new DesertNavigatorNode
+            {
+                Key = matches[0].Value,
+                Left = matches[1].Value,
+                Right = matches[2].Value
+            });
+        }
+        var startingNodes =
+            nodes
+                .Where(node => node.Key[2] == 'A').ToList();
+
+        var movesNodesNeed = startingNodes
+            .Select(node => GetMovesNeededBetter(nodes, node, directions))
+            .ToList();
+        for (long i = movesNodesNeed.Max();; i+=movesNodesNeed.Max())
+        {
+            if (i % 10000000== 0)
+            {
+                Console.WriteLine($"moves so far : {i}");
+            }
+            if (movesNodesNeed.All(node => i % node == 0))
+            {
+                return i;
+            }
+        }
     }
+    
+    public static int Navigate(List<string> navigationInstructions)
+    {
+        var directions = navigationInstructions[0];
+        var nodes = new List<DesertNavigatorNode>();
+        for (int i = 2; i < navigationInstructions.Count; i++)
+        {
+            var matches = _capitalLettersRegex.Matches(navigationInstructions[i]);
+            nodes.Add(new DesertNavigatorNode
+            {
+                Key = matches[0].Value,
+                Left = matches[1].Value,
+                Right = matches[2].Value
+            });
+        }
+
+        var currentNode = nodes.First(node => node.Key == "AAA");
+        return GetMovesNeeded(nodes, currentNode, directions);
+    }
+
+    private static int GetMovesNeeded(List<DesertNavigatorNode> allNodes, DesertNavigatorNode currentNode,
+        string directions)
+    {
+        var moves = 0;
+        while (true)
+        {
+            foreach (var direction in directions)
+            {
+                moves++;
+                if (direction == 'L')
+                {
+                    currentNode = allNodes.First(node => node.Key == currentNode.Left);
+                }
+
+                if (direction == 'R')
+                {
+                    currentNode = allNodes.First(node => node.Key == currentNode.Right);
+                }
+
+                if (currentNode.Key == "ZZZ")
+                {
+                    return moves;
+                }
+            }
+        }
+    }
+
+    private static int GetMovesNeededBetter(List<DesertNavigatorNode> allNodes, DesertNavigatorNode currentNode,
+        string directions)
+    {
+        var moves = 0;
+        while (true)
+        {
+            foreach (var direction in directions)
+            {
+                moves++;
+                if (direction == 'L')
+                {
+                    currentNode = allNodes.First(node => node.Key == currentNode.Left);
+                }
+
+                if (direction == 'R')
+                {
+                    currentNode = allNodes.First(node => node.Key == currentNode.Right);
+                }
+
+                if (currentNode.Key[2] == 'Z')
+                {
+                    return moves;
+                }
+            }
+        }
+    }
+}
+
+internal class DesertNavigatorNode
+{
+    public string Key { get; set; }
+    public string Left { get; set; }
+    public string Right { get; set; }
+}
